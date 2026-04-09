e988985ff6d4ae548128dbef11b2bf6af5596ef4
Bartixxx32 <mcbplay1@gmail.com>
2026-04-09 20:22:57 +0200

    Bump version to 1.3.2 (12) for F-Droid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bartixxx.oneplusarbchecker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.3.1"
+        versionCode = 12
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
