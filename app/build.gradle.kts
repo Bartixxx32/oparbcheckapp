e2cdbe9c1c077b47763aa8f6986630cd00e1e342
Bartixxx32 <mcbplay1@gmail.com>
2026-04-24 23:24:15 +0200

    Feat: Region conversion detection (v1.4.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bartixxx.oneplusarbchecker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.4.0"
+        versionCode = 16
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
