bbbe87ceb4d64e4a86d372322f646fe7a15778e0
Bartixxx32 <mcbplay1@gmail.com>
2026-05-14 03:46:26 +0200

    Fix: Remove obsolete Widevine strings causing Lint errors (v1.5.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bartixxx.oneplusarbchecker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.5.1"
+        versionCode = 25
+        versionName = "1.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
