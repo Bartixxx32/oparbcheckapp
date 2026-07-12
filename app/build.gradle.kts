52a15b0edafe75e42620169897c6cab1cc5260e6
Bartixxx32 <mcbplay1@gmail.com>
2026-07-12 21:44:29 +0200

    bump 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bartixxx.oneplusarbchecker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 36
-        versionName = "1.7.0"
+        versionCode = 37
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
